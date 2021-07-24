0af433b79beb128a0e24e186b636efc4ec720308
Pedro Abreu <pedroliveirabreu@gmail.com>
2021-07-24 18:03:25 -0300

    created home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,29 @@
 import Head from 'next/head';
+import Image from 'next/image';
+
+import avatarImg from '../../public/images/avatar.svg';
+
+import styles from '../styles/home.module.scss';
 
 export default function Home() {
   return (
     <>
       <Head>
-        <title>pt.news | Home</title>
+        <title>ig.news | Home</title>
       </Head>
-      <h1>Hello World</h1>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, welcome</span>
+          <h1>
+            News about the <span>React</span> world.
+          </h1>
+          <p>
+            Get access to all the publications <br />
+            <span>for $9.90 month</span>
+          </p>
+        </section>
+        <Image src={avatarImg} alt="Girl coding" />
+      </main>
     </>
   );
 }
