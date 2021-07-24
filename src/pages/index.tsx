5c8d1c42d5a44ca43bdaab450127e1f689ee3945
Pedro Abreu <pedroliveirabreu@gmail.com>
2021-07-24 20:53:28 -0300

    added stripe to project and get product data by ssr

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import { GetServerSideProps } from 'next';
 import Head from 'next/head';
 import Image from 'next/image';
 
@@ -5,9 +6,18 @@ import avatarImg from '../../public/images/avatar.svg';
 
 import { SubscribeButton } from '../components/SubscribeButton';
 
+import { stripe } from '../services/stripe';
+
 import styles from '../styles/home.module.scss';
 
-export default function Home() {
+interface HomeProps {
+  product: {
+    priceId: string;
+    amount: number;
+  };
+}
+
+export default function Home({ product }: HomeProps) {
   return (
     <>
       <Head>
@@ -21,12 +31,32 @@ export default function Home() {
           </h1>
           <p>
             Get access to all the publications <br />
-            <span>for $9.90 month</span>
+            <span>for {product.amount} month</span>
           </p>
-          <SubscribeButton />
+          <SubscribeButton priceId={product.priceId} />
         </section>
         <Image src={avatarImg} alt="Girl coding" />
       </main>
     </>
   );
 }
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const stripeProduct = await stripe.prices.retrieve(
+    'price_1JGtryBnlASF3ytPueiryLnx'
+  );
+
+  const product = {
+    priceId: stripeProduct.id,
+    amount: new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD',
+    }).format(stripeProduct.unit_amount / 100),
+  };
+
+  return {
+    props: {
+      product,
+    },
+  };
+};
